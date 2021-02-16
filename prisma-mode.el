54aa157488c5efb16dcc5365ff533a3322844179
Julius de Bruijn <julius+github@nauk.io>
2021-02-16 14:52:12 +0100

    Initial commit

---
+++ prisma-mode.el
@@ -0,0 +1,49 @@
+;;; prisma-mode.el --- A major mode for editing Prisma data model files. -*- coding: utf-8; lexical-binding: t; -*-
+
+;; Copyright © 2021, by Julius de Bruijn
+
+;; Author: Julius de Bruijn ( bruijn@prisma.io )
+;; Version: 0.1
+;; Created: 16 Feb 2021
+;; Keywords: languages
+;; Homepage: https://github.com/prisma/emacs-prisma-mode/
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; You can redistribute this program and/or modify it under the terms of the GNU General Public License version 2.
+
+;;; Commentary:
+;;; Code:
+
+(setq prisma-font-lock-keywords
+      (let* (
+             ;; We match `model Album {', but hilight only the word `model'.
+             ;; Same rules for `enum`, `datasource` and `type'.
+             (x-keywords-regexp "^\s*\\(model\\|enum\\|datasource\\|type\\)\s+[a-zA-Z0-9_-]+\s*{")
+             (x-scalar-types-regexp "^\s+[a-zA-Z0-9_-]+\s+\\(Int\\|String\\|Boolean\\|DateTime\\|Float\\|Decimal\\|FJson\\)")
+             (x-field-attributes-regexp "\@\\(id\\|map\\|default\\|relation\\|unique\\|index\\|ignore\\)")
+             (x-block-attributes-regexp "\@@\\(id\\|map\\|default\\|relation\\|unique\\|index\\|ignore\\)")
+             (x-native-types-regexp "\@[a-zA-Z0-9_-]+\.[a-zA-Z]+")
+             (x-properties-regexp "[a-zA-Z_-]+:")
+             (x-attribute-functions-regexp "[(]\s*\\(autoincrement\\|cuid\\|uuid\\|now\\|dbgenerated\\)\(\)\s*[)]")
+             )
+        `(
+          (,x-block-attributes-regexp . font-lock-preprocessor-face)
+          (,x-field-attributes-regexp . font-lock-preprocessor-face)
+          (,x-attribute-functions-regexp . (1 font-lock-function-name-face))
+          (,x-native-types-regexp . font-lock-preprocessor-face)
+          (,x-keywords-regexp . (1 font-lock-keyword-face))
+          (,x-properties-regexp . font-lock-variable-name-face)
+          (,x-scalar-types-regexp . (1 font-lock-type-face))
+          )))
+
+(define-derived-mode prisma-mode js-mode "prisma mode"
+  "Major mode for editing Prisma data models."
+
+  (setq-default indent-tabs-mode nil)
+  (setq tab-width 2)
+  (setq font-lock-defaults '((prisma-font-lock-keywords))))
+
+(provide 'prisma-mode)
